df5b5e3c74689956f3e1d2925fdff6431d266775
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-05 01:47:35 +0800

    Fix import statement to follow PEP8 guidelines and improve clarity

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+
 class LearningPathModel:
     def __init__(self, data):
         self.data = data
