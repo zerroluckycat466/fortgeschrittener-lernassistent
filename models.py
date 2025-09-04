213b491cb1d4315b59b388f8cd97d4e57f10ac12
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-05 01:45:09 +0800

    feat: initial commit for models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+class LearningPathModel:
+    def __init__(self, data):
+        self.data = data
+        self.model = RandomForestClassifier()
+
+    def train_model(self):
+        X = self.data.drop('target', axis=1)
+        y = self.data['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, user_data):
+        return self.model.predict(user_data)
